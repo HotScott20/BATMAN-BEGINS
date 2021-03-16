0d484a5c3dff27b05165565180705daf98de8e77
HotScott20 <74575163+HotScott20@users.noreply.github.com>
2021-03-15 20:05:52 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+var maxDrops = 100;
+for(var i=0; i<maxDrops, i++){
+    drops.push(new createDrop(random(0, 400), random(0, 400)));
+}
+var = raindrops
+
+function preload(){
+    
+}
+
+function setup(){
+   
+    
+}
+
+function draw(){
+    createCanvas 100, 500;
+    raindrops = createSprite(50, 50)
+    raindrops.velocityY = 3;
+
+}   
+
